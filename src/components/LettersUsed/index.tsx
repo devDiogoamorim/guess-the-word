0debb9e2b002783197ced8f488133b13ae0c4a80
devDiogoamorim <diogoamorimv1011@gmail.com>
2025-03-07 11:38:49 -0300

    first commit

---
+++ src/components/LettersUsed/index.tsx
@@ -0,0 +1,32 @@
+import { Letter } from "../Letter"
+import styles from "./styles.module.css"
+
+export type LetterUsedProps = {
+  value: string
+  correct: boolean
+}
+
+type Props = {
+  data: LetterUsedProps[]
+}
+
+export function LettersUsed({ data }: Props){
+  return(
+    <div className={styles.lettersUsed}>
+      <h5>Letras utilizadas</h5>
+      
+      <div>
+        {
+          data.map(({value, correct}) => (
+            <Letter 
+            key={value}
+            value={value} 
+            size="small" 
+            color={correct ? "correct" : "wrong"}
+            />
+          ))
+        }
+      </div>
+    </div>
+  )
+}
